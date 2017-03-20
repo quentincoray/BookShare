84fa35a00f407d041f42b55125c0e12bdc045dd1
Heninapso <heninapso@gmail.com>
2017-03-20 17:48:13 +0100

    home page with search form ok

--- app/controllers/bookmates_controller.rb
+++ app/controllers/bookmates_controller.rb
@@ -1,2 +1,14 @@
 class BookmatesController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: [:search]
+
+  def search
+    @address = params[:search]
+    @bookmates = Bookmate.near(@address, 10)
+    @hash = Gmaps4rails.build_markers(@bookmates) do |bookmate, marker|
+      marker.lat bookmate.latitude
+      marker.lng bookmate.longitude
+      marker.infowindow render_to_string(partial: 'infowindow', locals: { bookmate: bookmate })
+    end
+  end
 end
