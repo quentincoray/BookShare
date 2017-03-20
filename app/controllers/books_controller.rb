94935049d6235f5266add6d7109d1aa27e8a7fcc
Camille Franceschi <franceschi.camille@gmail.com>
2017-03-20 19:32:48 +0100

    working on

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+
+  def show
+    authorize @book
+  end
+
+  private
+
+  def set_book
+    @book = Bookm.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :bookmates, only: [:show]
+  resources :bookmates, only: [:show] do
+    resources :selling_books, only: [:show]
+  end
+  resources :books, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
