05acddc11ca174a3e7a619f19c343b862dbb212b
Quentin Coray <quentin.coray@gmail.com>
2017-03-20 18:43:35 +0100

    Added user#show route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :bookmates, only: [:show]
   mount Attachinary::Engine => "/attachinary"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:show]
 end
