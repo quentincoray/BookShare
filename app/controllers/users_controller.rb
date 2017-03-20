5c1faa2fa1ef32069c6e03e7db7545eb54ed306d
Camille Franceschi <franceschi.camille@gmail.com>
2017-03-20 19:49:00 +0100

    partly done with book page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:product).permit(:name, :description, :photo)
+    params.require(:user).permit(:name, :description, :photo)
   end
 end
