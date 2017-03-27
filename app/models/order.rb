794b123544fb67120bd7591b522cfa784cd50d5b
Camille Franceschi <franceschi.camille@gmail.com>
2017-03-27 16:23:43 +0200

    rating appears on bookmates search

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,4 +9,26 @@ class Order < ApplicationRecord
   validates :order_status, inclusion: { in: ["pending","paid"], allow_nil: false }
   validates :delivery_status, inclusion: { in: ["en cours","effectuée"], allow_nil: false }
   validates :delivery_type, inclusion: { in: ["livraison à domicile","livraison en main propre"], allow_nil: false }
+
+
+  # def average_rating
+  #   @mean = 0
+
+
+
+
+
+  #   @bookmate_orders = Order.all.where(:bookmate @bookmate)
+  #   @mean = 0
+  #   @bookmate_orders.each do |bookmate_order|
+  #     @sum += bookmate_order.rating
+  #   end
+  #   if @bookmate_orders.count > 0
+  #     @mean = @sum.fdiv(@bookmate_orders.count)
+  #   else
+  #     @mean = ""
+  #   end
+  #   @nb = @bookmate_orders.count
+  # end
+
 end
