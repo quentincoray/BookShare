95070850f8bc5f2129976367edee94876ecbdddf
Quentin Coray <quentin.coray@gmail.com>
2017-03-27 19:40:28 +0200

    Fixing Heroku bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-  { host: ENV["HOST"] || "localhost:3000" }
+  { host: ENV["HOST"] || "www.bookshare-lewagon.herokuapp.com" }
   end
 end
