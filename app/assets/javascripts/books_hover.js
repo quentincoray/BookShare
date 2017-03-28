ae3cd6e2e9e653ec68b85932f1c3690db02f969e
Camille Franceschi <franceschi.camille@gmail.com>
2017-03-28 23:02:46 +0200

    repare syntax

--- app/assets/javascripts/books_hover.js
+++ app/assets/javascripts/books_hover.js
@@ -1,4 +1,4 @@
-$(document).ready((function){
+$(document).ready(function() {
   $('.card').on('mouseenter', function(){
     $(this).nextAll().addClass('has-positive-translate')
     $(this).prevAll().addClass('has-negative-translate')
