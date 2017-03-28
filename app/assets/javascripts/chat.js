b9d1976a59ffb15ba90f8e67ab50e17ef1f3f2a8
Heninapso <heninapso@gmail.com>
2017-03-28 23:06:00 +0200

    new message displayed in a bubble

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -3,12 +3,19 @@ $(document).ready(function() {
   $('.listing').on('click', function () {
     $('.conversation').addClass('hidden')
     var content = $(this).text()
-    console.log(content);
+    // console.log(content);
     var conversation_id = $(this).data('target');
     console.log(conversation_id);
     $(conversation_id).removeClass('hidden');
   });
-
+  // $('.btn-primary').on('click', function() {
+  //   var previous_messages = $('#previous-messages').data('target');
+  //   console.log(previous_messages);
+  //   var new_message = $('#message-to-send').val();
+  //   console.log(new_message);
+  //   $('#new_chat_message').html(new_message);
+  //   $('#new_chat_message').slideDown();
+  // });
     // this.$textarea = $('#message-to-send');
     // addMessage: function() {
     //   this.messageToSend = this.$textarea.val()
