c9b672cca8c736234c83c9dcb833dfec7681b480
Heninapso <heninapso@gmail.com>
2017-03-28 18:06:48 +0200

    new strategy to post message with JS to be investigated

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -7,7 +7,20 @@ $(document).ready(function() {
     var conversation_id = $(this).data('target');
     console.log(conversation_id);
     $(conversation_id).removeClass('hidden');
-
-
   });
+
+    // this.$textarea = $('#message-to-send');
+    // addMessage: function() {
+    //   this.messageToSend = this.$textarea.val()
+    //   this.render();
+    // },
+    // addMessageEnter: function(event) {
+    //     // enter was pressed
+    //     if (event.keyCode === 13) {
+    //       this.addMessage();
+    //     }
+    // },
+    // scrollToBottom: function() {
+    //    this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
+    // },
 });
