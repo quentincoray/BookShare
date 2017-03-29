c5cb5b99bc0b024141d54b208b715d3264ae1e4c
Heninapso <heninapso@gmail.com>
2017-03-29 09:33:18 +0200

    save before merging new master

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,33 +1,19 @@
 $(document).ready(function() {
   $('#messages-content').hide;
   $('.listing').on('click', function () {
+    // Change active tab
+    $('.listing').removeClass('active');
+    $(this).addClass('active');
+    // Hide all tab-content (use class="hidden")
     $('.conversation').addClass('hidden')
-    var content = $(this).text()
+    // var content = $('.conversation').text();
+    // console.log(content);
     // console.log(content);
     var conversation_id = $(this).data('target');
     console.log(conversation_id);
-    $(conversation_id).removeClass('hidden');
+    // var conv_id_content = $(conversation_id).text();
+    // console.log(conv_id_content);
+    $(conversation_id).toggleClass('hidden');
   });
-  // $('.btn-primary').on('click', function() {
-  //   var previous_messages = $('#previous-messages').data('target');
-  //   console.log(previous_messages);
-  //   var new_message = $('#message-to-send').val();
-  //   console.log(new_message);
-  //   $('#new_chat_message').html(new_message);
-  //   $('#new_chat_message').slideDown();
-  // });
-    // this.$textarea = $('#message-to-send');
-    // addMessage: function() {
-    //   this.messageToSend = this.$textarea.val()
-    //   this.render();
-    // },
-    // addMessageEnter: function(event) {
-    //     // enter was pressed
-    //     if (event.keyCode === 13) {
-    //       this.addMessage();
-    //     }
-    // },
-    // scrollToBottom: function() {
-    //    this.$chatHistory.scrollTop(this.$chatHistory[0].scrollHeight);
-    // },
 });
+
