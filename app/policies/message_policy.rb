8040eca287574add83fef3749b732fe4f4715df9
Heninapso <heninapso@gmail.com>
2017-03-29 14:56:21 +0200

    chat fixed - maybe some CSS still to do

--- app/policies/message_policy.rb
+++ app/policies/message_policy.rb
@@ -1,7 +1,7 @@
 class MessagePolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope = Conversation.where(user1_id: user.id).or(Conversation.where(user2_id: user.id)).map { |conversation| conversation.messages }.flatten
+      scope = Conversation.where(user1_id: user.id).or(Conversation.where(user2_id: user.id)).map { |conversation| conversation.messages.order(created_at: :desc) }.flatten
 
     end
   end
