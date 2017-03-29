94cbac69723dbaebc597d2d005cc7ce239cad65e
Heninapso <heninapso@gmail.com>
2017-03-29 10:45:03 +0200

    message page displaying all messages when click on conversation

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -3,15 +3,23 @@ $(document).ready(function() {
   $('.listing').on('click', function () {
     // Change active tab
     $('.listing').removeClass('active');
-    $(this).addClass('active');
+    var active = $(this).addClass('active');
+    var conv_id = active.data('target');
+    console.log('hello');
+    console.log(conv_id);
+    // console.log(active);
     // Hide all tab-content (use class="hidden")
-    $('.conversation').addClass('hidden')
+    var hidden = $(conv_id).hasClass('hidden');
+    console.log(hidden);
+    $('.conversation').addClass('hidden');
     // var content = $('.conversation').text();
     // console.log(content);
     // console.log(content);
-    var conversation_id = $(this).data('target');
-    console.log(conversation_id);
-    $(conversation_id).toggleClass('hidden');
+    // var conversation_id = $(this).data('target');
+    // console.log(conversation_id);
+    if(hidden) {
+      $(conv_id).removeClass('hidden');
+     };
   });
 });
 
